94a3e006db6af8ae9224dc9d9ed81e4ee01fe0e6
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2024-04-12 12:26:11 +0200

    PYL-18 print firstname and lastname passed or informative errors

--- src/pylms/__main__.py
+++ src/pylms/__main__.py
@@ -1,8 +1,18 @@
 #!/bin/env python3
 
+from sys import argv
+
 
 def main() -> None:
-    print("Hello world!")
+    argv_length = len(argv)
+    if argv_length == 2:
+        print(f"Create Person {argv[1]}.")
+    elif argv_length == 3:
+        print(f"Create Person {argv[1]} {argv[2]}.")
+    elif argv_length > 3:
+        print(f"Too many arguments ({argv_length - 1})")
+    else:
+        print("Requires at least one argument")
 
 
 if __name__ == "__main__":
--- tests/pylms/main_test.py
+++ tests/pylms/main_test.py
@@ -3,7 +3,31 @@ from pylms import __main__
 
 
 @patch("builtins.print")
-def test_main(mock_print):
+def test_main_no_argument(mock_print):
     __main__.main()
 
-    mock_print.assert_called_with("Hello world!")
+    mock_print.assert_called_with("Requires at least one argument")
+
+
+@patch("builtins.print")
+@patch("pylms.__main__.argv", ["pylms", "A"])
+def test_main_one_argument(mock_print):
+    __main__.main()
+
+    mock_print.assert_called_with("Create Person A.")
+
+
+@patch("builtins.print")
+@patch("pylms.__main__.argv", ["pylms", "A", "B"])
+def test_main_two_arguments(mock_print):
+    __main__.main()
+
+    mock_print.assert_called_with("Create Person A B.")
+
+
+@patch("builtins.print")
+@patch("pylms.__main__.argv", ["pylms", "A", "B", "C"])
+def test_main_three_arguments(mock_print):
+    __main__.main()
+
+    mock_print.assert_called_with("Too many arguments (3)")

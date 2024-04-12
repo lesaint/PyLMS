57138887a156c0ec0885f5eb7684b9833dfaa74f
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2024-04-12 14:14:59 +0200

    PYL-18 factor code mocking argv for easier maintenance and readability

--- tests/pylms/main_test.py
+++ tests/pylms/main_test.py
@@ -10,6 +10,7 @@ def random_maj_char():
     return chr(randint(ord("A"), ord("Z")))
 
 
+# Fixtures
 @fixture
 def no_arguments():
     return []
@@ -30,29 +31,33 @@ def more_than_two_arguments():
     return [program_name] + [random_maj_char() for _ in range(1, 3 + randint(0, 5))]
 
 
+def mock_argv(arguments_list):
+    return patch("pylms.__main__.argv", [program_name] + arguments_list)
+
+
 @patch("builtins.print")
 def test_main_no_argument(mock_print, no_arguments):
-    with patch("pylms.__main__.argv", [program_name] + no_arguments):
+    with mock_argv(no_arguments):
         __main__.main()
         mock_print.assert_called_with("Requires at least one argument")
 
 
 @patch("builtins.print")
 def test_main_one_argument(mock_print, one_argument):
-    with patch("pylms.__main__.argv", [program_name] + one_argument):
+    with mock_argv(one_argument):
         __main__.main()
         mock_print.assert_called_with(f"Create Person {one_argument[0]}.")
 
 
 @patch("builtins.print")
 def test_main_two_arguments(mock_print, two_arguments):
-    with patch("pylms.__main__.argv", [program_name] + two_arguments):
+    with mock_argv(two_arguments):
         __main__.main()
         mock_print.assert_called_with(f"Create Person {two_arguments[0]} {two_arguments[1]}.")
 
 
 @patch("builtins.print")
 def test_main_three_arguments(mock_print, more_than_two_arguments):
-    with patch("pylms.__main__.argv", [program_name] + more_than_two_arguments):
+    with mock_argv(more_than_two_arguments):
         __main__.main()
         mock_print.assert_called_with(f"Too many arguments ({len(more_than_two_arguments)})")

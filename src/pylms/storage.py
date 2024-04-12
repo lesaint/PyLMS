7b627fbc01033ab841477e0a3b4655d1ce5c2122
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2024-04-12 18:33:53 +0200

    PYL-18 memorize more than one Person + fix display firstname-only Person
    * + introduce a domain
    * + separate parsing input argument from business logic

--- src/pylms/storage.py
+++ src/pylms/storage.py
@@ -1,22 +1,43 @@
 import json
 from pathlib import Path
+from pylms.core import Person
 
 file_name = "persons.db"
 
 
-def store_persons(persons: list[tuple[str, str]]) -> None:
+class PersonEncoder(json.JSONEncoder):
+    def default(self, o: any) -> any:
+        if isinstance(o, Person):
+            if o.lastname:
+                return {"firstname": o.firstname, "lastname": o.lastname}
+            return {"firstname": o.firstname}
+        # Let the base class default method raise the TypeError
+        return super().default(o)
+
+
+def to_Person(o: dict) -> Person:
+    if not "firstname" in o:
+        raise ValueError(f"Missing field 'firstname' type(o=={type(o)} o={str(o)}")
+
+    if "lastname" in o:
+        return Person(firstname=o["firstname"], lastname=o["lastname"])
+    return Person(firstname=o["firstname"])
+
+
+def store_persons(persons: list[Person]) -> None:
     if not persons:
         raise ValueError("Can't store an empty list of Persons.")
 
     with open(file_name, "w") as f:
-        f.write(json.dumps(persons))
+        f.write(json.dumps(persons, cls=PersonEncoder))
 
 
-def read_persons() -> list[tuple[str, str]]:
+def read_persons() -> list[Person]:
     file = Path(file_name)
     if file.exists():
         with file.open("r") as f:
             content = f.read()
             if content:
-                return json.loads(content)
-            return []
+                return [to_Person(s) for s in json.loads(content)]
+
+    return []

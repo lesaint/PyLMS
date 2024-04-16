1d341726253fc44f3965efb79d95ac5cf2ae3c90
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2024-04-16 12:51:55 +0200

    PYL-19 store Person.created

--- src/pylms/storage.py
+++ src/pylms/storage.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 from pathlib import Path
 from pylms.core import Person
@@ -9,21 +10,24 @@ class PersonEncoder(json.JSONEncoder):
     def default(self, o: any) -> any:
         if isinstance(o, Person):
             if o.lastname:
-                return {"id": o.person_id, "firstname": o.firstname, "lastname": o.lastname}
-            return {"id": o.person_id, "firstname": o.firstname}
+                return {"id": o.person_id, "firstname": o.firstname, "lastname": o.lastname, "created": str(o.created)}
+            return {"id": o.person_id, "firstname": o.firstname, "created": str(o.created)}
         # Let the base class default method raise the TypeError
         return super().default(o)
 
 
 def to_person(o: dict) -> Person:
-    if "id" not in o:
-        raise ValueError(f"Incompatible storage version: id field does not exist")
     if "firstname" not in o:
         raise ValueError(f"Missing field 'firstname' type(o=={type(o)} o={str(o)}")
 
     if "lastname" in o:
-        return Person(person_id=o["id"], firstname=o["firstname"], lastname=o["lastname"])
-    return Person(person_id=o["id"], firstname=o["firstname"])
+        return Person(
+            person_id=o["id"],
+            firstname=o["firstname"],
+            lastname=o["lastname"],
+            created=datetime.fromisoformat(o["created"]),
+        )
+    return Person(person_id=o["id"], firstname=o["firstname"], created=datetime.fromisoformat(o["created"]))
 
 
 def store_persons(persons: list[Person]) -> None:
--- tests/pylms/storage_test.py
+++ tests/pylms/storage_test.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pathlib import Path
 from pylms.core import Person
 from pylms.storage import read_persons, store_persons
@@ -21,7 +22,7 @@ def test_read_one_person_with_lastname(tmpdir):
     file_name = tmpdir + "foo.db"
     file = Path(file_name)
     with file.open("w"):
-        file.write_text('[{"id": 11, "firstname": "John", "lastname": "Doe"}]')
+        file.write_text('[{"id": 11, "firstname": "John", "lastname": "Doe", "created": "2024-04-16 12:40:30.407998"}]')
 
     with patch("pylms.storage.file_name", file_name):
         persons = read_persons()
@@ -29,13 +30,14 @@ def test_read_one_person_with_lastname(tmpdir):
         assert persons[0].person_id == 11
         assert persons[0].firstname == "John"
         assert persons[0].lastname == "Doe"
+        assert persons[0].created == datetime(2024, 4, 16, 12, 40, 30, 407998)
 
 
 def test_read_one_person_without_lastname(tmpdir):
     file_name = tmpdir + "foo.db"
     file = Path(file_name)
     with file.open("w"):
-        file.write_text(r'[{"id": 10, "firstname": "S\u00e9bastien"}]')
+        file.write_text(r'[{"id": 10, "firstname": "S\u00e9bastien", "created": "2024-04-16 12:40:30"}]')
 
     with patch("pylms.storage.file_name", file_name):
         persons = read_persons()
@@ -43,6 +45,7 @@ def test_read_one_person_without_lastname(tmpdir):
         assert persons[0].person_id == 10
         assert persons[0].firstname == "Sébastien"
         assert persons[0].lastname is None
+        assert persons[0].created == datetime(2024, 4, 16, 12, 40, 30)
 
 
 def test_store_empty_list():
@@ -53,31 +56,36 @@ def test_store_empty_list():
 def test_store_one_person_with_lastname(tmpdir):
     file_name = tmpdir + "foo.db"
     file = Path(file_name)
-    person = Person(1, "Jim", "Morisson")
+    person = Person(1, "Jim", "Morisson", created=datetime(2024, 4, 5, 12, 41, 9))
 
     with patch("pylms.storage.file_name", file_name):
         store_persons([person])
 
     with file.open("r"):
-        assert file.read_text() == '[{"id": 1, "firstname": "Jim", "lastname": "Morisson"}]'
+        assert (
+            file.read_text()
+            == '[{"id": 1, "firstname": "Jim", "lastname": "Morisson", "created": "2024-04-05 12:41:09"}]'
+        )
 
 
 def test_store_one_person_without_lastname(tmpdir):
     file_name = tmpdir + "foo.db"
     file = Path(file_name)
-    person = Person(2, "Jim")
+    person = Person(2, "Jim", created=datetime(2024, 4, 5, 12, 41, 58))
 
     with patch("pylms.storage.file_name", file_name):
         store_persons([person])
 
     with file.open("r"):
-        assert file.read_text() == '[{"id": 2, "firstname": "Jim"}]'
+        assert file.read_text() == '[{"id": 2, "firstname": "Jim", "created": "2024-04-05 12:41:58"}]'
 
 
 def test_store_persons(tmpdir):
     file_name = tmpdir + "foo.db"
     file = Path(file_name)
-    persons = [Person(3, "Sébastien", "Lesaint"), Person(4, "Max", "Payne"), Person(5, "Bob")]
+    t1 = datetime(2024, 4, 5, 12, 41, 58)
+    t2 = datetime(2024, 9, 18, 21, 8, 8)
+    persons = [Person(3, "Sébastien", "Lesaint", t1), Person(4, "Max", "Payne", t2), Person(5, "Bob", created=t2)]
 
     with patch("pylms.storage.file_name", file_name):
         store_persons(persons)
@@ -85,8 +93,8 @@ def test_store_persons(tmpdir):
     with file.open("r"):
         assert (
             file.read_text() == "["
-            r'{"id": 3, "firstname": "S\u00e9bastien", "lastname": "Lesaint"}, '
-            '{"id": 4, "firstname": "Max", "lastname": "Payne"}, '
-            '{"id": 5, "firstname": "Bob"}'
+            r'{"id": 3, "firstname": "S\u00e9bastien", "lastname": "Lesaint", "created": "2024-04-05 12:41:58"}, '
+            '{"id": 4, "firstname": "Max", "lastname": "Payne", "created": "2024-09-18 21:08:08"}, '
+            '{"id": 5, "firstname": "Bob", "created": "2024-09-18 21:08:08"}'
             "]"
         )

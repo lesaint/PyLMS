050f5d135b71c10d7d76d88fa5da051a33a00e97
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2024-04-16 12:30:27 +0200

    PYL-19 add created to Person class

--- tests/pylms/core_test.py
+++ tests/pylms/core_test.py
@@ -1,5 +1,28 @@
+from datetime import datetime
 from pylms.pylms import Person
 from pylms.pylms import PersonIdGenerator
+from unittest.mock import patch
+
+
+@patch("pylms.core.datetime.datetime")
+def test_created_not_provided_in_constructor(mock_datetime):
+    expected = datetime(2024, 4, 16, 2, 2, 2)
+    mock_datetime.now.return_value = expected
+
+    under_test = Person(person_id=1, firstname="f", lastname="n")
+
+    assert under_test.created == expected
+
+
+def test_created_provided_in_constructor():
+    expected = datetime(2024, 4, 16, 2, 2, 2)
+    other = datetime(2024, 4, 16, 10, 10, 10)
+
+    with patch("pylms.core.datetime.datetime") as mock_datetime:
+        mock_datetime.now.return_value = other
+        under_test = Person(person_id=1, firstname="f", lastname="n", created=expected)
+
+    assert under_test.created == expected
 
 
 def test_eq():
@@ -13,13 +36,22 @@ def test_eq():
     assert under_test != (1, "f", "n")
 
 
+def test_created_not_in_eq():
+    t1 = datetime(2023, 4, 16, 11, 52, 56)
+    t2 = datetime(2023, 4, 16, 11, 53, 15)
+
+    assert Person(person_id=1, firstname="f", lastname="n", created=t1) == Person(
+        person_id=1, firstname="f", lastname="n", created=t2
+    )
+
+
 def test_generator_no_person():
     under_test = PersonIdGenerator([])
 
     assert under_test.next_person_id() == 0
 
 
-def test_generator_no_person():
+def test_generator_one_person():
     under_test = PersonIdGenerator([Person(2, "n")])
 
     assert under_test.next_person_id() == 3

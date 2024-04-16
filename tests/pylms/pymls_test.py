123e9e7ccff834cec1a58256d055394e45ea620b
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2024-04-16 15:13:05 +0200

    PYL-19 sort Persons when listing

--- tests/pylms/pymls_test.py
+++ tests/pylms/pymls_test.py
@@ -100,9 +100,9 @@ def test_list_persons_non_empty_storage(mock_store_persons, mock_read_persons, m
     t1 = datetime(2024, 4, 5, 12, 41, 58)
     t2 = datetime(2024, 9, 18, 21, 8, 8)
     persons = [
+        Person(3, "Bob", created=t1),
         Person(1, "Seb", "King", t1),
         Person(2, "Mario", "Bros", t2),
-        Person(3, "Bob", created=t1),
     ]
 
     mock_read_persons.return_value = persons
@@ -114,9 +114,9 @@ def test_list_persons_non_empty_storage(mock_store_persons, mock_read_persons, m
     assert mock_store_persons.call_count == 0
     mock_print.assert_has_calls(
         [
-            call("(1)", persons[0], "(2024-4-5 12-41-58)"),
-            call("(2)", persons[1], "(2024-9-18 21-8-8)"),
-            call("(3)", persons[2], "(2024-4-5 12-41-58)"),
+            call("(1)", persons[1], "(2024-4-5 12-41-58)"),
+            call("(2)", persons[2], "(2024-9-18 21-8-8)"),
+            call("(3)", persons[0], "(2024-4-5 12-41-58)"),
         ]
     )
     assert mock_print.call_count == 3

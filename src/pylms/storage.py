c002d8e0a2faf73a78221dae9ab3fe85c7c0b1be
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2024-04-19 22:33:02 +0200

    PYL-24 save and display parent/enfant relationships

--- src/pylms/storage.py
+++ src/pylms/storage.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 import json
 from pathlib import Path
-from pylms.core import Person
+from pylms.core import Person, Relationship, relationship_definitions
 
-file_name = "persons.db"
+persons_file_name = "persons.db"
+relationships_file_name = "relationships.db"
 
 
 class PersonEncoder(json.JSONEncoder):
@@ -34,16 +35,73 @@ def store_persons(persons: list[Person]) -> None:
     if not persons:
         raise ValueError("Can't store an empty list of Persons.")
 
-    with open(file_name, "w") as f:
+    with open(persons_file_name, "w") as f:
         f.write(json.dumps(persons, cls=PersonEncoder))
 
 
 def read_persons() -> list[Person]:
-    file = Path(file_name)
+    file = Path(persons_file_name)
     if file.exists():
         with file.open("r") as f:
             content = f.read()
             if content:
                 return [to_person(s) for s in json.loads(content)]
+
+    return []
+
+
+class RelationshipEncoder(json.JSONEncoder):
+    def default(self, o: any) -> any:
+        if isinstance(o, Relationship):
+            return {"left": o.left.person_id, "right": o.right.person_id, "definition": o.definition.name}
+        # Let the base class default method raise the TypeError
+        return super().default(o)
+
+
+def _to_relationship(o: dict, persons: list[Person]) -> Relationship | None:
+    field_names = ("left", "right", "definition")
+    if not all(field_name in o for field_name in field_names):
+        raise ValueError(f"Missing at least one field of {field_names}")
+
+    left_id = int(o["left"])
+    right_id = int(o["right"])
+    definition_name = o["definition"]
+
+    person_index = {person.person_id: person for person in persons}
+    if left_id not in person_index or right_id not in person_index:
+        print(f"Either person {left_id} or person {right_id} does not exist")
+        return None
+
+    definition_index = {definition.name: definition for definition in relationship_definitions}
+    if definition_name not in definition_index:
+        print(f"definition {definition_name} does not exist")
+        return None
+
+    return Relationship(
+        person_left=person_index[left_id],
+        person_right=person_index[right_id],
+        definition=definition_index[definition_name],
+    )
+
+
+def store_relationships(relationships: list[Relationship]) -> None:
+    if not relationships:
+        raise ValueError("Can't store an empty list of Relationships.")
+
+    with open(relationships_file_name, "w") as f:
+        f.write(json.dumps(relationships, cls=RelationshipEncoder))
+
+
+def read_relationships(persons: list[Person]) -> list[Relationship]:
+    if not persons:
+        raise ValueError("Persons can't be empty")
+
+    file = Path(relationships_file_name)
+    if file.exists():
+        with file.open("r") as f:
+            content = f.read()
+            if content:
+                res = [_to_relationship(s, persons) for s in json.loads(content)]
+                return list(filter(lambda t: t is not None, res))
 
     return []
--- tests/pylms/storage_test.py
+++ tests/pylms/storage_test.py
@@ -1,20 +1,21 @@
 from datetime import datetime
 from pathlib import Path
-from pylms.core import Person
+from pylms.core import Person, Relationship, RelationshipDefinition
 from pylms.storage import read_persons, store_persons
-from pytest import raises
+from pylms.storage import read_relationships, store_relationships
+from pytest import raises, mark
 from unittest.mock import patch
 
 
 def test_read_persons_no_file(tmpdir):
-    with patch("pylms.storage.file_name", tmpdir + "foo.db"):
+    with patch("pylms.storage.persons_file_name", tmpdir + "foo.db"):
         assert not read_persons()
 
 
 def test_read_persons_empty_file(tmpdir):
     file_name = tmpdir + "foo.db"
     Path(file_name).touch()
-    with patch("pylms.storage.file_name", file_name):
+    with patch("pylms.storage.persons_file_name", file_name):
         assert not read_persons()
 
 
@@ -24,7 +25,7 @@ def test_read_one_person_with_lastname(tmpdir):
     with file.open("w"):
         file.write_text('[{"id": 11, "firstname": "John", "lastname": "Doe", "created": "2024-04-16 12:40:30.407998"}]')
 
-    with patch("pylms.storage.file_name", file_name):
+    with patch("pylms.storage.persons_file_name", file_name):
         persons = read_persons()
         assert len(persons) == 1
         assert persons[0].person_id == 11
@@ -39,7 +40,7 @@ def test_read_one_person_without_lastname(tmpdir):
     with file.open("w"):
         file.write_text(r'[{"id": 10, "firstname": "S\u00e9bastien", "created": "2024-04-16 12:40:30"}]')
 
-    with patch("pylms.storage.file_name", file_name):
+    with patch("pylms.storage.persons_file_name", file_name):
         persons = read_persons()
         assert len(persons) == 1
         assert persons[0].person_id == 10
@@ -48,7 +49,7 @@ def test_read_one_person_without_lastname(tmpdir):
         assert persons[0].created == datetime(2024, 4, 16, 12, 40, 30)
 
 
-def test_store_empty_list():
+def test_store_empty_list_of_persons():
     with raises(ValueError, match="Can't store an empty list of Persons."):
         store_persons([])
 
@@ -58,7 +59,7 @@ def test_store_one_person_with_lastname(tmpdir):
     file = Path(file_name)
     person = Person(1, "Jim", "Morisson", created=datetime(2024, 4, 5, 12, 41, 9))
 
-    with patch("pylms.storage.file_name", file_name):
+    with patch("pylms.storage.persons_file_name", file_name):
         store_persons([person])
 
     with file.open("r"):
@@ -73,7 +74,7 @@ def test_store_one_person_without_lastname(tmpdir):
     file = Path(file_name)
     person = Person(2, "Jim", created=datetime(2024, 4, 5, 12, 41, 58))
 
-    with patch("pylms.storage.file_name", file_name):
+    with patch("pylms.storage.persons_file_name", file_name):
         store_persons([person])
 
     with file.open("r"):
@@ -85,9 +86,13 @@ def test_store_persons(tmpdir):
     file = Path(file_name)
     t1 = datetime(2024, 4, 5, 12, 41, 58)
     t2 = datetime(2024, 9, 18, 21, 8, 8)
-    persons = [Person(3, "Sébastien", "Lesaint", t1), Person(4, "Max", "Payne", t2), Person(5, "Bob", created=t2)]
+    persons = [
+        Person(3, "Sébastien", "Lesaint", t1),
+        Person(4, "Max", "Payne", t2),
+        Person(5, "Bob", created=t2),
+    ]
 
-    with patch("pylms.storage.file_name", file_name):
+    with patch("pylms.storage.persons_file_name", file_name):
         store_persons(persons)
 
     with file.open("r"):
@@ -98,3 +103,184 @@ def test_store_persons(tmpdir):
             '{"id": 5, "firstname": "Bob", "created": "2024-09-18 21:08:08"}'
             "]"
         )
+
+
+def test_store_empty_list_of_relationships():
+    with raises(ValueError, match="Can't store an empty list of Relationships."):
+        store_relationships([])
+
+
+def test_store_one_relationship(tmpdir):
+    file_name = tmpdir + "foo.db"
+    file = Path(file_name)
+    person1 = Person(1, "Jim", "Morisson")
+    person2 = Person(2, "Paul", "John")
+    definition = RelationshipDefinition(name="rs_name")
+    relationship = Relationship(person_left=person1, person_right=person2, definition=definition)
+
+    with patch("pylms.storage.relationships_file_name", file_name):
+        store_relationships([relationship])
+
+    with file.open("r"):
+        assert file.read_text() == '[{"left": 1, "right": 2, "definition": "rs_name"}]'
+
+
+def test_store_relationships(tmpdir):
+    file_name = tmpdir + "foo.db"
+    file = Path(file_name)
+    person1 = Person(1, "Jim", "Morisson")
+    person2 = Person(2, "Paul", "John")
+    person3 = Person(3, "Tony", "Parker")
+    definition1 = RelationshipDefinition(name="rs1_name")
+    definition2 = RelationshipDefinition(name="rs2_name")
+
+    with patch("pylms.storage.relationships_file_name", file_name):
+        store_relationships(
+            [
+                Relationship(person_left=person1, person_right=person2, definition=definition1),
+                Relationship(person_left=person2, person_right=person3, definition=definition2),
+                Relationship(person_left=person3, person_right=person2, definition=definition1),
+                Relationship(person_left=person1, person_right=person3, definition=definition2),
+                Relationship(person_left=person1, person_right=person2, definition=definition1),
+            ]
+        )
+
+    with file.open("r"):
+        assert file.read_text() == (
+            "["
+            '{"left": 1, "right": 2, "definition": "rs1_name"}'
+            ", "
+            '{"left": 2, "right": 3, "definition": "rs2_name"}'
+            ", "
+            '{"left": 3, "right": 2, "definition": "rs1_name"}'
+            ", "
+            '{"left": 1, "right": 3, "definition": "rs2_name"}'
+            ", "
+            '{"left": 1, "right": 2, "definition": "rs1_name"}'
+            "]"
+        )
+
+
+def test_read_relationships_empty_list_of_persons():
+    with raises(ValueError, match="Persons can't be empty"):
+        read_relationships([])
+
+
+def test_read_relationships_no_file(tmpdir):
+    persons = [Person(2, "Foo", "Bar")]
+
+    with patch("pylms.storage.relationships_file_name", tmpdir + "foo.db"):
+        assert not read_relationships(persons)
+
+
+def test_read_relationships_empty_file(tmpdir):
+    persons = [Person(2, "Foo", "Bar")]
+    file_name = tmpdir + "foo.db"
+    Path(file_name).touch()
+
+    with patch("pylms.storage.relationships_file_name", file_name):
+        assert not read_relationships(persons)
+
+
+def test_read_one_relationship(tmpdir):
+    file_name = tmpdir + "foo.db"
+    persons = [
+        Person(2, "Foo", "Bar"),
+        Person(3, "Acme", "Donut"),
+    ]
+    relationship = RelationshipDefinition(name="rs1_name")
+    file = Path(file_name)
+    with file.open("w"):
+        file.write_text('[{"left": 3, "right": 2, "definition": "rs1_name"}]')
+
+    with (
+        patch("pylms.storage.relationships_file_name", file_name),
+        patch("pylms.storage.relationship_definitions", new=[relationship]),
+    ):
+        relationships = read_relationships(persons)
+        assert len(relationships) == 1
+        assert relationships[0].left == persons[1]
+        assert relationships[0].right == persons[0]
+        assert relationships[0].definition is relationship
+
+
+@mark.parametrize(
+    "serialized_relationship",
+    [
+        '{"left": 4, "right": 2, "definition": "rs1_name"}',
+        '{"left": 3, "right": 1, "definition": "rs1_name"}',
+        '{"left": 3, "right": 2, "definition": "does_not_exist"}',
+        '{"left": 4, "right": 2, "definition": "rs1_name"}',
+        '{"left": 1, "right": 4, "definition": "rs1_name"}',
+        '{"left": 10, "right": 12, "definition": "does_not_exist"}',
+    ],
+)
+def test_read_relationship_ignore_entry_if_person_or_definition_does_not_exist(serialized_relationship, tmpdir):
+    file_name = tmpdir + "foo.db"
+    persons = [
+        Person(2, "Foo", "Bar"),
+        Person(3, "Acme", "Donut"),
+    ]
+    relationship = RelationshipDefinition(name="rs1_name")
+    file = Path(file_name)
+    with file.open("w"):
+        file.write_text(f"[{serialized_relationship}]")
+
+    with (
+        patch("pylms.storage.relationships_file_name", file_name),
+        patch("pylms.storage.relationship_definitions", new=[relationship]),
+    ):
+        assert not read_relationships(persons)
+
+
+def test_read_relationships(tmpdir):
+    file_name = tmpdir + "foo.db"
+    persons = [
+        Person(1, "Jim", "Morisson"),
+        Person(2, "Paul", "John"),
+        Person(3, "Tony", "Parker"),
+    ]
+    relationship1 = RelationshipDefinition(name="rs1_name")
+    relationship2 = RelationshipDefinition(name="rs2_name")
+    file = Path(file_name)
+    with file.open("w"):
+        file.write_text(
+            "["
+            '{"left": 1, "right": 2, "definition": "rs1_name"}'
+            ", "
+            '{"left": 2, "right": 3, "definition": "rs2_name"}'
+            ", "
+            '{"left": 3, "right": 2, "definition": "rs1_name"}'
+            ", "
+            '{"left": 1, "right": 3, "definition": "rs2_name"}'
+            ", "
+            '{"left": 1, "right": 2, "definition": "rs1_name"}'
+            "]"
+        )
+
+    with (
+        patch("pylms.storage.relationships_file_name", file_name),
+        patch("pylms.storage.relationship_definitions", new=[relationship1, relationship2]),
+    ):
+        relationships = read_relationships(persons)
+        assert len(relationships) == 5
+
+        assert relationships[0].left == persons[0]
+        assert relationships[0].right == persons[1]
+        assert relationships[0].definition is relationship1
+
+        assert relationships[1].left == persons[1]
+        assert relationships[1].right == persons[2]
+        assert relationships[1].definition is relationship2
+
+        assert relationships[2].left == persons[2]
+        assert relationships[2].right == persons[1]
+        assert relationships[2].definition is relationship1
+
+        assert relationships[3].left == persons[0]
+        assert relationships[3].right == persons[2]
+        assert relationships[3].definition is relationship2
+
+        assert relationships[4].left == persons[0]
+        assert relationships[4].right == persons[1]
+        assert relationships[4].definition is relationship1

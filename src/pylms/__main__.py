a4d3586173ecb7b7a938a98e42e207b6817c8270
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2024-05-22 14:34:35 +0200

    PYL-38 make search the default command
    * "create" command must now be used to create a new Person

--- src/pylms/__main__.py
+++ src/pylms/__main__.py
@@ -20,83 +20,92 @@ def main() -> None:
 
 
 def _read_and_execute_commands() -> None:
-    argv_length = len(argv)
+    args: list[str] = argv[1:]
 
-    if argv_length == 1:
+    if not args:
         _command_list()
         return
 
-    if argv[1].lower() == "update":
-        _command_update(argv_length)
+    command = args[0].lower()
+    if command == "create":
+        _command_create(args[1:])
         return
 
-    if argv[1].lower() == "delete":
-        _command_delete(argv_length)
+    if command == "update":
+        _command_update(args[1:])
         return
 
-    if argv[1].lower() == "link":
-        _command_link(argv_length)
+    if command == "delete":
+        _command_delete(args[1:])
         return
 
-    if argv[1].lower() == "search":
-        _command_search(argv_length)
+    if command == "link":
+        _command_link(args[1:])
         return
 
-    _command_create(argv_length)
+    _command_search(args)
 
 
 def _command_list() -> None:
     list_persons()
 
 
-def _command_create(argv_length: int) -> None:
-    if argv_length > 3:
-        print(f"Too many arguments ({argv_length - 1})")
+def _command_create(arguments: list[str]) -> None:
+    arguments_count = len(arguments)
+    if arguments_count < 1:
+        print(f"Too few arguments ({arguments_count})")
+        return
+    if arguments_count > 2:
+        print(f"Too many arguments ({arguments_count})")
         return
 
-    if argv_length == 2:
-        store_person(firstname=argv[1])
+    if arguments_count == 1:
+        store_person(firstname=arguments[0])
     else:
-        store_person(firstname=argv[1], lastname=argv[2])
+        store_person(firstname=arguments[0], lastname=arguments[1])
 
 
-def _command_update(argv_length: int) -> None:
-    if argv_length > 3:
-        print(f"Too many arguments ({argv_length - 1})")
+def _command_update(arguments: list[str]) -> None:
+    arguments_count = len(arguments)
+    if arguments_count > 1:
+        print(f"Too many arguments ({arguments_count})")
         return
 
-    if argv_length == 3:
-        update_person(pattern=argv[2])
-    else:
-        print(f"Missing search pattern")
-
-
-def _command_delete(argv_length: int) -> None:
-    if argv_length > 3:
-        print(f"Too many arguments ({argv_length - 1})")
-        return
-
-    if argv_length == 3:
-        delete_person(pattern=argv[2])
+    if arguments_count == 1:
+        update_person(pattern=arguments[0])
     else:
         print(f"Missing search pattern")
 
 
-def _command_link(argv_length: int) -> None:
-    if argv_length < 4:
-        print(f"Too few arguments ({argv_length -1})")
+def _command_delete(arguments: list[str]) -> None:
+    arguments_count = len(arguments)
+    if arguments_count > 1:
+        print(f"Too many arguments ({arguments_count})")
         return
 
-    natural_link_request = " ".join(argv[2:])
+    if arguments_count == 1:
+        delete_person(pattern=arguments[0])
+    else:
+        print(f"Missing search pattern")
+
+
+def _command_link(arguments: list[str]) -> None:
+    arguments_count = len(arguments)
+    if arguments_count < 2:
+        print(f"Too few arguments ({arguments_count})")
+        return
+
+    natural_link_request = " ".join(arguments)
     link_persons(natural_link_request)
 
 
-def _command_search(argv_length: int) -> None:
-    if argv_length < 3:
-        print(f"Too few arguments ({argv_length})")
+def _command_search(arguments: list[str]) -> None:
+    arguments_count = len(arguments)
+    if arguments_count < 1:
+        print(f"Too few arguments ({arguments_count})")
         return
 
-    natural_search_request = " ".join(argv[2:])
+    natural_search_request = " ".join(arguments)
     search_persons(natural_search_request)
 
 
